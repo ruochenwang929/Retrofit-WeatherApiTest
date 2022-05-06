6a2b4feecdf293706ea2005fd1d752a71626d724
Ruochen Wang <dylan@Ruochen-Wang.local>
2022-05-07 00:15:40 +0800

    image change

--- app/src/main/java/com/example/weatherapitest/MainActivity.java
+++ app/src/main/java/com/example/weatherapitest/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.weatherapitest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 
@@ -25,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        int brightness = -35; //RGB偏移量，变暗为负数
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.set(new float[]{1, 0, 0, 0, brightness, 0, 1, 0, 0, brightness, 0, 0, 1, 0, brightness, 0, 0, 0, 1, 0});
+        ColorMatrixColorFilter cmcf = new ColorMatrixColorFilter(matrix);
+
+
         //The Retrofit class generates an implementation of the weatherApi interface.
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.openweathermap.org/data/2.5/")
@@ -42,21 +51,40 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<Root> call, Response<Root> response) { //response拿到的就是json文件
                 Root root = response.body();
                 double a = root.getMain().getTemp();
-                int b = (int)(a-273.15); //Change Kelvin to Celsius
+                int b = (int) (a - 273.15); //Change Kelvin to Celsius
                 String temp = String.valueOf(b);
-                binding.tempTextView.setText(temp+"℃");
+                binding.tempTextView.setText(temp + "℃");
 
                 String str = root.getWeather().get(0).getMain();
-                if (str.equals("Rain") || str.equals("Snow") || str.equals("Extreme")){
+                //str.equals("Snow") || str.equals("Extreme")
+                if (str.equals("Rain"))
+                {
+                    binding.weatherImage.setImageDrawable(getResources().getDrawable(R.drawable.rain));
+                    binding.mainTextView.setText(str + ", recommended indoor exercise");
+                }
+                else if (str.equals("Extreme"))
+                {
+                    binding.weatherImage.setImageDrawable(getResources().getDrawable(R.drawable.extreme));
+                    binding.mainTextView.setText(str + ", recommended indoor exercise");
+                }
+                else if (str.equals("Snow"))
+                {
+                    binding.weatherImage.setImageDrawable(getResources().getDrawable(R.drawable.snow));
+                    binding.weatherImage.setColorFilter(cmcf); //imageView为显示图片的View。
                     binding.mainTextView.setText(str + ", recommended indoor exercise");
                 }
                 else
+                {
+                    //clear
+                    binding.weatherImage.setImageDrawable(getResources().getDrawable(R.drawable.clear));
+                    binding.weatherImage.setColorFilter(cmcf); //imageView为显示图片的View。
                     binding.mainTextView.setText(str + ", recommended outdoor exercise");
+                }
             }
 
             @Override
             public void onFailure(Call<Root> call, Throwable t) {
-                System.out.println(t.getMessage());
+                System.out.println("Error," + t.getMessage());
             }
 
         });
